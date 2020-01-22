5c60ca2110e915f0f8a020363b2a8675eb4d18c3
ocd-cloud-lyon <60095830+ocd-cloud-lyon@users.noreply.github.com>
2020-01-22 11:39:10 +0100

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,6 +1,10 @@
-provider "aws" {}
+provider "aws" {
+  assume_role {
+    role_arn     = "arn:aws:iam::573329840855:role/AccessLyon"
+  }
+}
 
 # Create a VPC
-#resource "aws_vpc" "sma-example" {
-#  cidr_block = "10.70.0.0/16"
-#}
+resource "aws_vpc" "sma-example" {
+  cidr_block = "10.70.0.0/16"
+}
