c56aa16a8c91e8fefbcee400e3d768db09172d1d
ocd-cloud-lyon <60095830+ocd-cloud-lyon@users.noreply.github.com>
2020-01-24 14:37:03 +0100

    Update AWS_create_EC2_instance.tf

--- AWS_create_EC2_instance.tf
+++ AWS_create_EC2_instance.tf
@@ -1,29 +1,3 @@
-provider "aws" {
-  assume_role {
-    role_arn     = "arn:aws:iam::573329840855:role/AccessLyon"
-  }
-}
-
-
-resource "aws_default_security_group" "default" {
-   vpc_id      = "vpc-03cca642ef84627e2"
- ingress {
-     from_port   = 0
-     to_port     = 0
-     protocol    = "-1"
-     # Please restrict your ingress to only necessary IPs and ports.
-     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-     cidr_blocks     = ["0.0.0.0/0"]
-   }
- egress {
-     from_port       = 0
-     to_port         = 0
-     protocol        = "-1"
-     cidr_blocks     = ["0.0.0.0/0"]
-   }
- }
-
-
 resource "aws_instance" "test-EC2-in-VPC" {
   ami                    = "ami-007fae589fdf6e955" #id of desired AMI
   instance_type          = "t2.small"
