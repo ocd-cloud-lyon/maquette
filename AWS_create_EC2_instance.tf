5d0990853d63ced7ad0661686cf0b83d186253cb
ocd-cloud-lyon <60095830+ocd-cloud-lyon@users.noreply.github.com>
2020-01-27 11:55:15 +0100

    Update AWS_create_EC2_instance.tf

--- AWS_create_EC2_instance.tf
+++ AWS_create_EC2_instance.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "test-EC2-in-VPC" {
+resource "aws_instance" "test-EC2" {
   ami                    = "ami-007fae589fdf6e955" #id of desired AMI
   instance_type          = "t2.small"
   subnet_id              = "subnet-0599b65bdbcc15355"
