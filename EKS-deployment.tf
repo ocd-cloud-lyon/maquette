1898d1eb1656ce6fd5aab23da15331e354bdd1c9
ocd-cloud-lyon <60095830+ocd-cloud-lyon@users.noreply.github.com>
2020-01-28 13:41:04 +0100

    Add files via upload

---
+++ EKS-deployment.tf
@@ -0,0 +1,281 @@
+#Définition des variables
+variable "cluster-name" {
+  default = "eks-deployment"
+  type    = string
+}
+
+#Pour nous la partie VPC est déjà créée !
+
+#Mise en place du rôle IAM directement lié au service EKS
+resource "aws_iam_role" "eks-cluster" {
+  name = "eks-cluster"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2020-01-24",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = "${aws_iam_role.eks-node.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cluster-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = "${aws_iam_role.eks-node.name}"
+}
+
+resource "aws_security_group" "eks-cluster" {
+  name        = "eks-cluster"
+  description = "Cluster communication with worker nodes"
+  vpc_id      = "vpc-03cca642ef84627e2"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-cluster"
+  }
+}
+
+# OPTIONAL: Allow inbound traffic from your local workstation external IP
+#           to the Kubernetes. You will need to replace A.B.C.D below with
+#           your real IP. Services like icanhazip.com can help you find this.
+resource "aws_security_group_rule" "eks-cluster-ingress-workstation-https" {
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Allow workstation to communicate with the cluster API Server"
+  from_port         = 443
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.eks-cluster.id}"
+  to_port           = 443
+  type              = "ingress"
+}
+
+resource "aws_eks_cluster" "eks-cluster" {
+  name            = "${var.cluster-name}"
+  role_arn        = "${aws_iam_role.eks-node.arn}"
+
+  vpc_config {
+    security_group_ids = ["${aws_security_group.eks-cluster.id}"]
+    subnet_ids         = ["vpc-03cca642ef84627e2"]
+  }
+
+  depends_on = [
+    "aws_iam_role_policy_attachment.eks-cluster-AmazonEKSClusterPolicy",
+    "aws_iam_role_policy_attachment.eks-cluster-AmazonEKSServicePolicy",
+  ]
+}
+#Partie configuration de notre cluster Kubernetes
+locals {
+  kubeconfig = <<KUBECONFIG
+apiVersion: v1
+clusters:
+- cluster:
+    server: ${aws_eks_cluster.eks-cluster.endpoint}
+    certificate-authority-data: ${aws_eks_cluster.eks-cluster.certificate_authority.0.data}
+  name: kubernetes
+contexts:
+- context:
+    cluster: kubernetes
+    user: aws
+  name: aws
+current-context: aws
+kind: Config
+preferences: {}
+users:
+- name: aws
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1alpha1
+      command: aws-iam-authenticator
+      args:
+        - "token"
+        - "-i"
+        - "${var.cluster-name}"
+KUBECONFIG
+}
+
+output "kubeconfig" {
+  value = "${local.kubeconfig}"
+}
+
+resource "aws_iam_role" "eks-node" {
+  name = "eks-node"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+
+}
+
+resource "aws_iam_role_policy_attachment" "eks-node-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-node.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-node-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks-node.name
+}
+
+resource "aws_security_group" "eks-node" {
+  name        = "eks-node"
+  description = "Security group for all nodes in the cluster"
+  vpc_id      = "vpc-03cca642ef84627e2"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Name"                                      = "eks-node"
+    "kubernetes.io/cluster/${var.cluster-name}" = "owned"
+  }
+}
+
+resource "aws_security_group_rule" "eks-node-ingress-self" {
+  description              = "Allow node to communicate with each other"
+  from_port                = 0
+  protocol                 = "-1"
+  security_group_id        = aws_security_group.eks-node.id
+  source_security_group_id = aws_security_group.eks-node.id
+  to_port                  = 65535
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "eks-node-ingress-cluster" {
+  description              = "Allow worker Kubelets and pods to receive communication from the cluster control      plane"
+  from_port                = 1025
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks-node.id
+  source_security_group_id = aws_security_group.eks-node.id
+  to_port                  = 65535
+  type                     = "ingress"
+ }
+
+#Worker node access to the cluster 
+resource "aws_security_group_rule" "eks-cluster-ingress-node-https" {
+  description              = "Allow pods to communicate with the cluster API Server"
+  from_port                = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks-cluster.id
+  source_security_group_id = aws_security_group.eks-node.id
+  to_port                  = 443
+  type                     = "ingress"
+}
+
+#Worker node AutoScaling Group
+data "aws_ami" "eks-worker" {
+   filter {
+     name   = "name"
+     values = ["amazon-eks-node-${aws_eks_cluster.eks-cluster.version}-v*"]
+   }
+
+   most_recent = true
+   owners      = ["573329840855"] # Amazon EKS AMI Account ID
+ }
+
+ # This data source is included for ease of sample architecture deployment
+# and can be swapped out as necessary.
+data "aws_region" "current" {
+}
+
+# EKS currently documents this required userdata for EKS worker nodes to
+# properly configure Kubernetes applications on the EC2 instance.
+# We implement a Terraform local here to simplify Base64 encoding this
+# information into the AutoScaling Launch Configuration.
+# More information: https://docs.aws.amazon.com/eks/latest/userguide/launch-workers.html
+locals {
+  demo-node-userdata = <<USERDATA
+#!/bin/bash
+set -o xtrace
+/etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.eks-cluster.endpoint}' --b64-cluster-ca '${aws_eks_cluster. eks-cluster.certificate_authority[0].data}' '${var.cluster-name}'
+USERDATA
+
+}
+
+resource "aws_launch_configuration" "eks-cluster" {
+  associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.name
+  image_id                    = data.aws_ami.eks-worker.id
+  instance_type               = "m4.large"
+  name_prefix                 = "terraform-eks"
+  security_groups  = [aws_security_group.eks-cluster.id]
+  user_data_base64 = base64encode(local.eks-cluster-userdata)
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "eks-cluster" {
+  desired_capacity     = 2
+  launch_configuration = aws_launch_configuration.eks-cluster.id
+  max_size             = 2
+  min_size             = 1
+  name                 = "terraform-eks"
+  vpc_zone_identifier = [aws_subnet.*.id]
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-eks"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "kubernetes.io/cluster/${var.cluster-name}"
+    value               = "owned"
+    propagate_at_launch = true
+  }
+}
+
+locals {
+  config_map_aws_auth = <<CONFIGMAPAWSAUTH
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapRoles: |
+    - rolearn: ${aws_iam_role.eks-node.arn}
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+CONFIGMAPAWSAUTH
+
+}
+
+output "config_map_aws_auth" {
+  value = local.config_map_aws_auth
+}
