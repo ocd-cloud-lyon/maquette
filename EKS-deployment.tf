2d59c0de8b811e3c3cd3c8cbca384fdb0f7749f4
ocd-cloud-lyon <60095830+ocd-cloud-lyon@users.noreply.github.com>
2020-01-28 13:50:04 +0100

    Update EKS-deployment.tf

--- EKS-deployment.tf
+++ EKS-deployment.tf
@@ -230,7 +230,7 @@ resource "aws_launch_configuration" "eks-cluster" {
   instance_type               = "m4.large"
   name_prefix                 = "terraform-eks"
   security_groups  = [aws_security_group.eks-cluster.id]
-  user_data_base64 = base64encode(local.eks-cluster)
+  #user_data_base64 = base64encode(local.eks-cluster)
 
   lifecycle {
     create_before_destroy = true
