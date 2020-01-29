e30169c568e00fec6a5ba90cdeb4c7901a7e61a1
ocd-cloud-lyon <60095830+ocd-cloud-lyon@users.noreply.github.com>
2020-01-29 14:25:54 +0100

    Add files via upload

---
+++ EKS-security-group-rules-eks.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group_rule" "tf-eks-master-ingress-workstation-https" {
+  cidr_blocks       = ["${var.accessing_computer_ip}/32"]
+  description       = "Allow workstation to communicate with the cluster API Server."
+  from_port         = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.tf-eks-master.id
+  to_port           = 443
+  type              = "ingress"
+}
+resource "aws_security_group_rule" "tf-eks-node-ingress-workstation-https" {
+  cidr_blocks       = ["${var.accessing_computer_ip}/32"]
+  description       = "Allow workstation to communicate with the Kubernetes nodes directly."
+  from_port         = 22
+  protocol          = "tcp"
+  security_group_id = aws_security_group.tf-eks-node.id
+  to_port           = 22
+  type              = "ingress"
+}
