44b6fe81bb6002399208659f66b55c802ac43f90
ocd-cloud-lyon <60095830+ocd-cloud-lyon@users.noreply.github.com>
2020-01-29 16:11:07 +0100

    Add files via upload

---
+++ EKS-iam.tf
@@ -0,0 +1,25 @@
+# Setup for IAM role needed to setup an EKS cluster
+resource "aws_iam_role" "tf-eks-master" {
+  name = "terraform-eks-cluster"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+resource "aws_iam_role_policy_attachment" "tf-cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.tf-eks-master.name
+}
+resource "aws_iam_role_policy_attachment" "tf-cluster-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role
