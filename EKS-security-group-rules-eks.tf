5d586a2fbdbedeb069b58c52513c7cb02b0f73d4
ocd-cloud-lyon <60095830+ocd-cloud-lyon@users.noreply.github.com>
2020-01-29 15:58:38 +0100

    Update EKS-security-group-rules-eks.tf

--- EKS-security-group-rules-eks.tf
+++ EKS-security-group-rules-eks.tf
@@ -22,8 +22,8 @@ resource "aws_security_group_rule" "tf-eks-node-ingress-self" {
   description              = "Allow nodes to communicate with each other"
   from_port                = 0
   protocol                 = "-1"
-  security_group_id        = "${aws_security_group.tf-eks-node.id}"
-  source_security_group_id = "${aws_security_group.tf-eks-node.id}"
+  security_group_id        = aws_security_group.tf-eks-node.id
+  source_security_group_id = aws_security_group.tf-eks-node.id
   to_port                  = 65535
   type                     = "ingress"
 }
@@ -31,8 +31,8 @@ resource "aws_security_group_rule" "tf-eks-node-ingress-cluster" {
   description              = "Allow worker Kubelets and pods to receive communication from the cluster control plane"
   from_port                = 1025
   protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.tf-eks-node.id}"
-  source_security_group_id = "${aws_security_group.tf-eks-master.id}"
+  security_group_id        = aws_security_group.tf-eks-node.id
+  source_security_group_id = aws_security_group.tf-eks-master.id
   to_port                  = 65535
   type                     = "ingress"
 }
@@ -41,8 +41,8 @@ resource "aws_security_group_rule" "tf-eks-cluster-ingress-node-https" {
   description              = "Allow pods to communicate with the cluster API Server"
   from_port                = 443
   protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.tf-eks-node.id}"
-  source_security_group_id = "${aws_security_group.tf-eks-master.id}"
+  security_group_id        = aws_security_group.tf-eks-node.id
+  source_security_group_id = aws_security_group.tf-eks-master.id
   to_port                  = 443
   type                     = "ingress"
 }
@@ -50,8 +50,8 @@ resource "aws_security_group_rule" "tf-eks-node-ingress-master" {
   description              = "Allow cluster control to receive communication from the worker Kubelets"
   from_port                = 443
   protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.tf-eks-master.id}"
-  source_security_group_id = "${aws_security_group.tf-eks-node.id}"
+  security_group_id        = aws_security_group.tf-eks-master.id
+  source_security_group_id = aws_security_group.tf-eks-node.id
   to_port                  = 443
   type                     = "ingress"
 }
