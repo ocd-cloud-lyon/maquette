85885fdb5aa2154bc612fc0b314187bbbdc86120
ocd-cloud-lyon <60095830+ocd-cloud-lyon@users.noreply.github.com>
2020-01-29 10:59:17 +0100

    Update EKS-security-group.tf

--- EKS-security-group.tf
+++ EKS-security-group.tf
@@ -8,8 +8,4 @@ resource "aws_security_group" "tf-eks-master" {
         protocol    = "-1"
         cidr_blocks = ["0.0.0.0/0"]
       }
-   
-      tags {
-        Name = "terraform-eks"
-      }
 }
