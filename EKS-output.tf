13c18dadbdec588e65e7cd598d8b87783add302e
ocd-cloud-lyon <60095830+ocd-cloud-lyon@users.noreply.github.com>
2020-02-03 10:35:27 +0100

    Add files via upload

---
+++ EKS-output.tf
@@ -0,0 +1,12 @@
+#On parle de l'Output du VPC
+output "vpc_id" {
+  value = aws_vpc.example.id
+}
+
+#Ici, on parle de l'output du Kubeconfig
+output "eks_kubeconfig" {
+  value = "${local.kubeconfig}"
+  depends_on = [
+    "aws_eks_cluster.tf_eks."
+  ]
+}
