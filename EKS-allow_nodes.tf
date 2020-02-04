1217e30cea83e3dc30360f7970ca48ca749cd1b2
ocd-cloud-lyon <60095830+ocd-cloud-lyon@users.noreply.github.com>
2020-02-04 09:18:04 +0100

    Update EKS-allow_nodes.tf

--- EKS-allow_nodes.tf
+++ EKS-allow_nodes.tf
@@ -16,13 +16,12 @@ resource "kubernetes_config_map" "aws_auth" {
     namespace = "kube-system" 
   } 
   data { 
-    mapRoles = <<EOF 
+    mapRoles =  
 - rolearn: ${aws_iam_role.tf-eks-node.arn} 
   username: system:node:{{EC2PrivateDNSName}} 
   groups: 
     - system:bootstrappers 
     - system:nodes 
-EOF 
   } 
   depends_on = [ 
     "aws_eks_cluster.tf_eks"  ] 
