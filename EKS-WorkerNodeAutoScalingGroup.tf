ea2cb4652e903b97e1073bc75eee44ed12ff0a83
ocd-cloud-lyon <60095830+ocd-cloud-lyon@users.noreply.github.com>
2020-02-06 15:28:37 +0100

    Update EKS-WorkerNodeAutoScalingGroup.tf

--- EKS-WorkerNodeAutoScalingGroup.tf
+++ EKS-WorkerNodeAutoScalingGroup.tf
@@ -1,14 +1,57 @@
-resource "aws_autoscaling_group" "demo" {
+data "aws_ami" "eks-worker-node" {
+   filter {
+     name   = "name"
+     values = ["amazon-eks-node-${aws_eks_cluster.eks.version}-v*"]
+   }
+
+   most_recent = true
+   owners      = ["573329840855"] # Amazon EKS AMI Account ID
+ }
+
+ # This data source is included for ease of sample architecture deployment
+# and can be swapped out as necessary.
+data "aws_region" "current" {
+}
+
+# EKS currently documents this required userdata for EKS worker nodes to
+# properly configure Kubernetes applications on the EC2 instance.
+# We implement a Terraform local here to simplify Base64 encoding this
+# information into the AutoScaling Launch Configuration.
+# More information: https://docs.aws.amazon.com/eks/latest/userguide/launch-workers.html
+locals {
+  demo-node-userdata = <<USERDATA
+#!/bin/bash
+set -o xtrace
+/etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.eks.endpoint}' --b64-cluster-ca '${aws_eks_cluster. eks.certificate_authority[0].data}' '${var.cluster-name}'
+USERDATA
+
+}
+
+resource "aws_launch_configuration" "eks-worker-node" {
+  associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.worker-node.name
+  image_id                    = data.aws_ami.eks-worker-node.id
+  instance_type               = "m4.large"
+  name_prefix                 = "terraform-eks"
+  security_groups  = [aws_security_group.worker-node.id]
+  user_data_base64 = base64encode(local.worker-node-userdata)
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "eks-worker-node" {
   desired_capacity     = 2
   launch_configuration = aws_launch_configuration.demo.id
   max_size             = 2
   min_size             = 1
   name                 = "terraform-eks-demo"
-  vpc_zone_identifier = [aws_subnet.demo.*.id]
+  #vpc_zone_identifier = [aws_subnet.demo.*.id]
 
   tag {
     key                 = "Name"
-    value               = "terraform-eks-demo"
+    value               = "terraform-eks"
     propagate_at_launch = true
   }
 
