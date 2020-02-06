c6b6899ec2495cecc04ad68cd809fb2080fba7df
ocd-cloud-lyon <60095830+ocd-cloud-lyon@users.noreply.github.com>
2020-02-06 12:41:30 +0100

    Add files via upload

---
+++ EKS-MasterCluster.tf
@@ -0,0 +1,15 @@
+resource "aws_eks_cluster" "demo" {
+  name            = "${var.cluster-name}"
+  role_arn        = "${aws_iam_role.master-node.arn}"
+
+  vpc_config {
+    security_group_ids = ["sg-08f14b983890ad155"]
+    subnet_ids         = ["subnet-0c187777047b53659",
+                          "vpc-03cca642ef84627e2"]
+  }
+
+  depends_on = [
+    "aws_iam_role_policy_attachment.eks-cluster-AmazonEKSClusterPolicy",
+    "aws_iam_role_policy_attachment.eks-cluster-AmazonEKSServicePolicy",
+  ]
+}
