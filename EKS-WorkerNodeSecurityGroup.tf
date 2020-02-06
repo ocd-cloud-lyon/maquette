0c4bf3106c00b629c93068eb23cc8dd5c60818a6
ocd-cloud-lyon <60095830+ocd-cloud-lyon@users.noreply.github.com>
2020-02-06 15:12:13 +0100

    Update EKS-WorkerNodeSecurityGroup.tf

--- EKS-WorkerNodeSecurityGroup.tf
+++ EKS-WorkerNodeSecurityGroup.tf
@@ -34,3 +34,4 @@ resource "aws_security_group_rule" "worker-node-ingress-cluster" {
   source_security_group_id = aws_security_group.eks-cluster.id
   to_port                  = 65535
   type                     = "ingress"
+  }
