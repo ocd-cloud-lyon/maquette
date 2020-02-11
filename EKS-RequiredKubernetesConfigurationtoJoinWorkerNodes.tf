c2c6f45e5b264c8803d6c0b3d9f8f1e504775a5b
ocd-cloud-lyon <60095830+ocd-cloud-lyon@users.noreply.github.com>
2020-02-11 19:09:29 +0100

    Update EKS-RequiredKubernetesConfigurationtoJoinWorkerNodes.tf

--- EKS-RequiredKubernetesConfigurationtoJoinWorkerNodes.tf
+++ EKS-RequiredKubernetesConfigurationtoJoinWorkerNodes.tf
@@ -9,7 +9,7 @@ metadata:
   namespace: kube-system
 data:
   mapRoles: |
-    - rolearn: ${aws_iam_role.worker-node.arn}
+    - rolearn = "terraform-eks-cluster"
       username: system:node:{{EC2PrivateDNSName}}
       groups:
         - system:bootstrappers
