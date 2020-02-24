598c767470a3f1b96b0ef751934f9b771e8dedb0
ocd-cloud-lyon <60095830+ocd-cloud-lyon@users.noreply.github.com>
2020-02-24 07:46:59 +0100

    Update AWS_create_EC2_instance.tf

--- AWS_create_EC2_instance.tf
+++ AWS_create_EC2_instance.tf
@@ -1,10 +1,10 @@
-resource "aws_instance" "test-EC2" {
+resource "aws_instance" "EKS-worker-01" {
   ami                    = "ami-0b248d53709a335ce" #id of desired AMI
   instance_type          = "t2.small"
   subnet_id              = "subnet-0c187777047b53659"
   key_name               = "SMA-KEY"
   tags = {
-    Name = "EC2-test"
+    Name = "EKS-worker-01"
   }
 }
 
