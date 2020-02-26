aeb01402aad66feff457910fe751e52049a858e6
ocd-cloud-lyon <60095830+ocd-cloud-lyon@users.noreply.github.com>
2020-02-26 16:45:17 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Bonjour Les amis\n");
+  response.end("Bonjour Les Amis\n");
 });
 
 // listen on localhost:8000
